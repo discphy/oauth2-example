75d5a0ede3e6784eeba00fd96895e7433f6de99e
discphy <dinophy@nate.com>
2023-07-13 08:41:34 +0900

    Initial commit

---
+++ src/main/java/com/discphy/example/oauth2/Oauth2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.discphy.example.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discphy/example/oauth2/Oauth2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discphy.example.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
