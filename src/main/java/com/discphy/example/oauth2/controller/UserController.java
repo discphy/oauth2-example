6816d3026443347602bd47421380d47fb15d494f
discphy <dinophy@nate.com>
2023-07-17 04:17:00 +0700

    feat(add): User controller added

---
+++ src/main/java/com/discphy/example/oauth2/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.discphy.example.oauth2.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public OAuth2User user(@AuthenticationPrincipal OAuth2User user) {
+        return user;
+    }
+}
