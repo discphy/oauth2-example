065d4438097bc8a1c7124e0bbbae79046593c202
discphy <dinophy@nate.com>
2023-07-19 10:34:49 +0700

    feat(add): Oidc user service

---
+++ src/main/java/com/discphy/example/oauth2/userinfo/service/CustomOidcUserService.java
@@ -0,0 +1,20 @@
+package com.discphy.example.oauth2.userinfo.service;
+
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOidcUserService extends OidcUserService {
+
+    @Override
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUser(userRequest);
+
+        // TODO : DB를 통한 회원검증
+
+        return oidcUser;
+    }
+}
